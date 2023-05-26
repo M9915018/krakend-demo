f584b0024a1c86d281dff7211cedd44f07632fbb
DESKTOP-98Q0G1M\IC <c8653120@yahoo.com.tw>
2023-05-26 15:26:23 +0800

    增加使用 GetHostname和GetIP

--- sample-app/main.go
+++ sample-app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 	"strconv"
 	"time"
 )
@@ -20,4 +22,36 @@ func main() {
 	fmt.Println("call_key_id:", call_key_id)
 	fmt.Println("時差:", now_utc.UnixNano()-now.UnixNano())
 
+	fmt.Println("GetHostname:", GetHostname())
+	fmt.Println("GetIP:", GetIP())
+}
+
+func GetHostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("get hostname failed, err = ", err.Error())
+		return ""
+	}
+	return hostname
+}
+
+func GetIP() string {
+	ifaces, _ := net.Interfaces()
+	var ip net.IP
+	// handle err
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		// handle err
+		for _, addr := range addrs {
+			// var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			// fmt.Println("get ip", ip)
+		}
+	}
+	return ip.String()
 }
